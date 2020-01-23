bd2d57b7ecd8d9b8a884732c67a4a86afec12955
Dillon Coffman <tug40394@temple.edu>
2020-01-23 16:32:10 -0500

    Check user permission to access location

--- app/src/main/java/edu/temple/gps_app_1_23_20/MainActivity.java
+++ app/src/main/java/edu/temple/gps_app_1_23_20/MainActivity.java
@@ -2,6 +2,9 @@ package edu.temple.gps_app_1_23_20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -23,5 +26,56 @@ public class MainActivity extends AppCompatActivity {
         lon = findViewById(R.id.textView2);
 
         locationManager = getSystemService(LocationManager.class);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                lat.setText(String.valueOf(location.getLatitude()));
+                lon.setText(String.valueOf(location.getLongitude()));
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Android generated permission check for Alt + Enter hint
+//        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    Activity#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for Activity#requestPermissions for more details.
+//            return;
+//        }
+
+        // Permission check
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // GPS needs a Provider, specify thresholds (minTime = how much time should transpire before you can get another update)f
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            // Request permission
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
     }
 }
