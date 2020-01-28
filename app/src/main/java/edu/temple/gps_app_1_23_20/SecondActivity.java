b6d1ad2688a0a7684c3ab236610997c012f5d206
Dillon Coffman <tug40394@temple.edu>
2020-01-28 16:19:59 -0500

    Adding permission to our GPS app

---
+++ app/src/main/java/edu/temple/gps_app_1_23_20/SecondActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.gps_app_1_23_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
